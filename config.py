58c63008d879a5671c49c764e0070b5259f187e3
bing188025 <q.legned.dev@gmail.com>
2026-02-28 14:03:33 -0800

    feat: added new function

--- config.py
+++ config.py
@@ -12,3 +12,7 @@ load_dotenv()
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 WELCOME_CHANNEL_ID = int(os.environ["WELCOME_CHANNEL_ID"])
+EMAIL_ADDRESS = os.environ["EMAIL_ADDRESS"]
+EMAIL_PASSWORD = os.environ["EMAIL_PASSWORD"]
+IMAP_SERVER = os.environ.get("IMAP_SERVER", "imap.gmail.com")
+CROWDWORKS_CHANNEL_ID = int(os.environ["CROWDWORKS_CHANNEL_ID"])
